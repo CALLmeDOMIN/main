fbbc02a79f9f2f0e5936c0fa77f4fb14e769e447
CALLmeDOMIN <dominiksieron12@gmail.com>
2021-08-04 14:33:00 +0200

    change to cnv.cpp

--- C++/cnv.cpp/cnv.cpp
+++ C++/cnv.cpp/cnv.cpp
@@ -1,18 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int *tab;
+int tab[100007];
 int main()
 {
-    int n = 0;
+    int n;
     cin >> n;
-    int x;
+    int x = 0;
     for (int i = 0; n > 0; i++)
     {
         tab[i] = n % 10;
         n /= 10;
         x++;
     }
+    cout << "[";
     for (int i = 0; i < x; i++)
     {
-        cout << tab[i] << "\n";
+        cout << tab[i];
+        if (i != x - 1)
+        {
+            cout << ",";
+        }
     }
+    cout << "]";
+}
